21bb7d2d08df998883e10b48235be0b39463fc38
EC2 Default User <ec2-user@ip-172-31-11-222.ap-northeast-1.compute.internal>
2023-06-07 06:31:42 +0000

    ヘッダーフッターサインアップログイン

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   resources :comments, only: [:index, :show]
   resource :members, only: [:edit, :update]
   get '/mypage' => 'members#show'
-  
-  
+  # get '/reviews' => 'reviews#index'
+  # get '/review_comments' => 'comments#create'
+  # get '/new_review' => 'reviews#new'
+
+
   namespace :public do
     resources :reviews do
       resources :comments, only: [:create, :destroy]
@@ -13,19 +16,19 @@ Rails.application.routes.draw do
     end
   end
 
-  
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :members,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    
+
 end
--- db/migrate/20230605141323_devise_create_members.rb
+++ db/migrate/20230605141323_devise_create_members.rb
@@ -32,6 +32,9 @@ class DeviseCreateMembers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name, null: false
+      t.string :nick_name, null: false
+
 
       t.timestamps null: false
     end
